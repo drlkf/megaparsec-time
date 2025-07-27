fcab952d7f6331dd8b169cb6ef70ab08edec910b
drlkf <drlkf@drlkf.net>
2025-07-28 01:06:28 +0200

    feat: initial release

---
+++ .releaserc.mjs
@@ -0,0 +1,42 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ["main"],
+    tagFormat: "${version}",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "semantic-release-mirror-version",
+            {
+                "fileGlob": "@(package.yaml|megaparsec-time.cabal)",
+                "placeholderRegExp": "0.0.0-dev"
+            }
+        ],
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md",
+                changelogTitle: "Changelog"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: [
+                    "CHANGELOG.md",
+                ]
+            }
+        ],
+        "@semantic-release/github",
+        [
+            "semantic-release-hackage",
+            {
+                "packageName": "megaparsec-time",
+                "versionPrefix": "0.",
+                "publishDocumentation": true
+            }
+        ]
+    ]
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# megaparsec-time
+Time parsers to use with the
+[Megaparsec](https://hackage.haskell.org/package/megaparsec) library.
+
+# Running tests
+Make sure your build passes tests using [Stack](https://docs.haskellstack.org/en/stable/).
+
+# Contributing
+This repository uses [Conventional
+Commits](https://www.conventionalcommits.org/en/v1.0.0/) to generate versions
+with [Semantic Release](https://github.com/semantic-release/semantic-release),
+so make sure your commits are compliant. Pull requests are rebased, not
+squashed, so make sure your commits make sense as well.
+
+It is preferrable that you format code using
+[fourmolu](https://github.com/fourmolu/fourmolu), a configuration is available
+at the root of the repository.
